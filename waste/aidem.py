69b329d8a35089ac81520706556797ff7a89b622
latrokles <latrokles@gmail.com>
2024-10-20 21:11:17 -0700

    feat: replace aidem's mpv usage with yt-dlp and ffplay... for now

--- waste/aidem.py
+++ waste/aidem.py
@@ -21,20 +21,16 @@ def main():
 CMD_HELP = """
 Supported commands:
 - /search
-- /play-audio
-- /play-video
-- /pause
-- /resume
-- /stop
+- /play
 - /quit
 - help
 """
 
 
 class Aidem(code.InteractiveConsole):
+    PLAY_CMD = "yt-dlp -f mp4 '{url}' -o - | ffplay - -autoexit -loglevel quiet"
     def __init__(self, *args):
         super().__init__(args)
-        self.mpv = MPVProcess()
         self.yt = YTMediaFinder()
         self.results = []
 
@@ -47,16 +43,8 @@ class Aidem(code.InteractiveConsole):
         match cmd:
             case "/search":
                 self.do_search(arg)
-            case "/play-audio":
-                self.do_play-audio(arg)
-            case "/play-video":
-                self.do_play_video(arg)
-            case "/pause":
-                self.do_pause()
-            case "/resume":
-                self.do_resume()
-            case "/stop":
-                self.do_stop()
+            case "/play":
+                self.do_play(arg)
             case "/quit":
                 self.do_quit()
             case "/help":
@@ -75,40 +63,27 @@ class Aidem(code.InteractiveConsole):
         for i, r in enumerate(self.results):
             print(f"{format_index(i)} -- {r.title}\n      {r.playback_url}\n")
 
-    def do_play_audio(self, index):
-        index = int(index)
-        r = self.results[index - 1]
-        print(f"Playing Audio for {index}: {r.title}({r.playback_url})")
-        self.mpv.send(["set_property", "video", "no"])
-        self.mpv.send(["loadfile", r.playback_url])
+    def do_play(self, index):
+        index = int(index) - 1
+        media = self.results[index]
 
-    def do_play_video(self, index):
-        index = int(index)
-        r = self.results[index - 1]
-        print(f"Playing Audio for {index}: {r.title}({r.playback_url})")
-        self.mpv.send(["set_property", "video", "auto"])
-        self.mpv.send(["loadfile", r.playback_url])
+        yt_dlp_cmd = ["yt-dlp", "-f", "mp4", media.playback_url, "-o", "-"]
+        ffplay_cmd = ["ffplay", "-", "-autoexit", "-loglevel", "quiet"]
 
-    def do_pause(self):
-        self.mpv.send(["set_property", "pause", True])
-
-    def do_resume(self):
-        self.mpv.send(["set_property", "pause", False])
-
-    def do_stop(self):
-        print("Stopping...")
-        self.mpv.send(["stop"])
+        with Popen(yt_dlp_cmd, stdout=PIPE) as yt_dlp_proc:
+            with Popen(ffplay_cmd, stdin=yt_dlp_proc.stdout):
+                print(f"Playing {media.title}({media.playback_url}")
+                yt_dlp_proc.stdout.close()
+                yt_dlp_proc.wait()
 
     def do_quit(self):
-        self.mpv.stop()
+        raise SystemExit()
 
     def do_help(self):
         print(CMD_HELP)
 
     def start(self):
-        self.mpv.start()
         self.interact(banner="Aidem - a distraction free player...", exitmsg="bye!")
-        self.mpv.stop()
 
 
 class MPVProcess:
@@ -127,10 +102,32 @@ class MPVProcess:
         self.ipc = socket.socket(socket.AF_UNIX)
         self.ipc.connect(MPVProcess.SOCK)
 
-    def stop(self):
+    def quit(self):
         if self.process is None:
             return
         self.process.kill()
+
+    def play_audio(self, url_or_pathname):
+        print(f"Playing Audio for {url_or_pathname}")
+        self.play(url_or_pathname, "no")
+
+    def play_video(self, url_or_pathname):
+        print(f"Playing Audio for {url_or_pathname}")
+        self.play(url_or_pathname, "auto")
+
+    def play(self, url_or_pathname, video_prop):
+        self.send(["set_property", "video", video_prop])
+        self.send(["loadfile", r.playback_url])
+
+    def pause(self):
+        self.send(["set_property", "pause", True])
+
+    def resume(self):
+        self.send(["set_property", "pause", False])
+
+    def stop(self):
+        print("Stopping...")
+        self.send(["stop"])
 
     def send(self, command):
         message = (

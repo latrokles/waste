1c0ac7a07474193f2ed6474073794c1cd1dadf5e
latrokles <latrokles@gmail.com>
2024-11-05 07:42:56 -0800

    refactor: begin extracting gui from draw functionality
    separate these concerns a bit more as one could do draw operations
    on a form that is not displayed and then store or print out the
    results.

---
+++ waste/gui.py
@@ -0,0 +1,283 @@
+import ctypes
+
+import sdl2
+
+from waste import draw
+
+DEFAULT_WIDHT = 64 * 8
+DEFAULT_HEIGHT = 32 * 8
+DEFAULT_ZOOM = 2
+DEFAULT_FPS = 30
+
+
+class MinimumWindow:
+    def __init__(
+        self,
+        title,
+        width=DEFAULT_WIDHT,
+        height=DEFAULT_HEIGHT,
+        zoom=DEFAULT_ZOOM,
+        fps=DEFAULT_FPS,
+        background=None,
+        window_opts=0,
+    ):
+        self.w = width
+        self.h = height
+        self.zoom = zoom
+        self.fps = fps
+
+        self.background = (background or draw.BLACK)
+        self.screen = draw.Form(0, self.w, self.h)  # TODO use factory method
+
+        if sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO) < 0:
+            err = f"Cannot initialize SDL, err={sdl2.SDL_GetError()}"
+            raise RuntimeError(err)
+
+        title = f"{title} - ({self.w} x {self.h} : {self.zoom})"
+        self.window = sdl2.SDL_CreateWindow(
+            title.encode("utf-8"),
+            sdl2.SDL_WINDOWPOS_UNDEFINED,
+            sdl2.SDL_WINDOWPOS_UNDEFINED,
+            self.w * self.zoom,
+            self.h * self.zoom,
+            window_opts,
+        )
+
+        self.renderer = sdl2.render.SDL_CreateRenderer(
+            self.window,
+            -1,
+            sdl2.SDL_RENDERER_PRESENTVSYNC,
+        )
+
+        self.texture = sdl2.SDL_CreateTexture(
+            self.renderer,
+            sdl2.SDL_PIXELFORMAT_RGBA8888,
+            sdl2.SDL_TEXTUREACCESS_STREAMING,
+            self.w,
+            self.h,
+        )
+
+        sdl2.SDL_SetWindowMinimumSize(self.window, self.w, self.h)
+        sdl2.render.SDL_RenderSetLogicalSize(self.renderer, self.w, self.h)
+        sdl2.render.SDL_RenderSetIntegerScale(self.renderer, 1)
+
+        sdl2.SDL_StartTextInput()
+
+    @property
+    def position(self):
+        x, y = ctypes.c_int(0), ctypes.c_int(0)
+        sdl2.SDL_GetWindowPosition(self.window, x, y)
+        return int(x.value), int(y.value)
+
+    def move(self, x, y):
+        sdl2.SDL_SetWindowPosition(self.window, x, y)
+
+    def enable_always_on_top(self):
+        sdl2.SDL_SetWindowAlwaysOnTop(self.window, True)
+
+    def disable_always_on_top(self):
+        sdl2.SDL_SetWindowAlwaysOnTop(self.window, False)
+
+
+class Window(MinimumWindow):
+    def __init__(
+        self,
+        title,
+        width=DEFAULT_WIDHT,
+        height=DEFAULT_HEIGHT,
+        zoom=DEFAULT_ZOOM,
+        fps=DEFAULT_FPS,
+        is_resizable=True,
+        has_border=True,
+        background=None,
+        start_on_create=True,
+    ):
+
+        self.w = width
+        self.h = height
+        self.zoom = zoom
+        self.fps = fps
+
+        if background is None:
+            background = BLACK
+        self.background = background
+
+        self.pixels = Form(0, 0, self.w, self.h)
+
+        window_opts = 0
+        if is_resizable:
+            window_opts |= sdl2.SDL_WINDOW_RESIZABLE
+
+        if not has_border:
+            window_opts |= sdl2.SDL_WINDOW_BORDERLESS
+
+        if sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO) < 0:
+            err = f"Cannot initialize SDL, err={sdl2.SDL_GetError()}"
+            raise RuntimeError(err)
+
+        title = f"{title} - ({self.w} x {self.h} : {self.zoom})"
+        self.window = sdl2.SDL_CreateWindow(
+            title.encode("utf-8"),
+            sdl2.SDL_WINDOWPOS_UNDEFINED,
+            sdl2.SDL_WINDOWPOS_UNDEFINED,
+            self.w * self.zoom,
+            self.h * self.zoom,
+            window_opts,
+        )
+
+        self.renderer = sdl2.render.SDL_CreateRenderer(
+            self.window,
+            -1,
+            sdl2.SDL_RENDERER_PRESENTVSYNC,
+        )
+
+        self.texture = sdl2.SDL_CreateTexture(
+            self.renderer,
+            sdl2.SDL_PIXELFORMAT_RGBA8888,
+            sdl2.SDL_TEXTUREACCESS_STREAMING,
+            self.w,
+            self.h,
+        )
+
+        sdl2.SDL_SetWindowMinimumSize(self.window, self.w, self.h)
+        sdl2.render.SDL_RenderSetLogicalSize(self.renderer, self.w, self.h)
+        sdl2.render.SDL_RenderSetIntegerScale(self.renderer, 1)
+
+        sdl2.SDL_StartTextInput()
+        print(self.position)
+        if start_on_create:
+            self.run()
+
+    @property
+    def size(self):
+        return (self.w, self.h)
+
+    def run(self):
+        next_tick = 0
+        while True:
+            tick = sdl2.SDL_GetTicks()
+            if tick < next_tick:
+                sdl2.SDL_Delay(next_tick - tick)
+
+            next_tick = tick + int(1_000 / self.fps)
+            event = sdl2.SDL_Event()
+            while sdl2.SDL_PollEvent(event):
+                self.dispatch_event(event)
+        self.quit()
+
+    def dispatch_event(self, event):
+        match event.type:
+            case sdl2.SDL_QUIT:
+                self.quit()
+            case (
+                sdl2.SDL_MOUSEMOTION | sdl2.SDL_MOUSEBUTTONUP | sdl2.SDL_MOUSEBUTTONDOWN
+            ):
+                self.handle_mouse(event)
+            case sdl2.SDL_KEYDOWN:
+                self.handle_key(event)
+            case sdl2.SDL_TEXTINPUT:
+                self.handle_text(event)
+            case sdl2.SDL_DROPFILE:
+                self.handle_drop(event)
+            case sdl2.SDL_CLIPBOARDUPDATE:
+                self.handle_clipboard_update(event)
+            case sdl2.SDL_WINDOWEVENT:
+                if event.window.event == sdl2.SDL_WINDOWEVENT_EXPOSED:
+                    self.redraw()
+            case _:
+                pass
+
+    def handle_mouse(self, event):
+        print("handling mouse event")
+        self.redraw()
+
+    def handle_key(self, event):
+        print("handling key event")
+        self.redraw()
+
+    def handle_text(self, event):
+        print("handling text event")
+        self.redraw()
+
+    def handle_drop(self, event):
+        print("handling drop event")
+        print(event.drop.file)
+        self.redraw()
+
+    def handle_clipboard_update(self, event):
+        print("handling clipboard update")
+
+    def clear(self):
+        if self.background == BLACK:
+            self.pixels.clear()
+            return
+        self.pixels.fill(self.background)
+
+    def redraw(self):
+        sdl2.SDL_UpdateTexture(
+            self.texture, None, self.pixels.bytes, self.pixels.w * self.pixels.depth
+        )
+        sdl2.SDL_RenderClear(self.renderer)
+        sdl2.SDL_RenderCopy(self.renderer, self.texture, None, None)
+        sdl2.SDL_RenderPresent(self.renderer)
+
+    def quit(self):
+        sdl2.SDL_StopTextInput()
+        sdl2.SDL_DestroyTexture(self.texture)
+        sdl2.SDL_DestroyRenderer(self.renderer)
+        sdl2.SDL_DestroyWindow(self.window)
+        sdl2.SDL_Quit()
+        sys.exit(0)
+
+    @debugmethod
+    def draw_glyph(self, x, y, glyph, w, h, fg_color, bg_color):
+        for row in range(h):
+            for col in range(w):
+                is_set = (glyph[row] >> ((w - 1) - col)) & 0x1
+
+                val = bg_color
+                if is_set:
+                    val = fg_color
+
+                self.put_pixel(x + col, y + row, val)
+
+    @debugmethod
+    def draw_image(self, x, y, image):
+        src_col_start = 0
+        src_col_end = image.w
+        src_row_start = 0
+        src_row_end = image.h
+
+        if image.w > self.w:
+            src_col_end = min(self.w + 1, image.w)
+
+        if image.h > self.h:
+            src_row_end = min(self.h + 1, image.h)
+
+        dst_col_start = x
+        dst_col_end = x + image.w
+        dst_row_start = y
+        dst_row_end = y + image.h
+
+        if dst_col_end >= self.w:
+            dst_col_end = dst_col_end - (dst_col_end - self.w)
+
+        if dst_row_end >= self.h:
+            dst_row_end = dst_row_end - (dst_row_end - self.h)
+
+        for row in range(src_row_end):
+            self.pixels.put_row_bytes(
+                src_col_start,
+                row,
+                image.row_bytes(0, row, src_col_end),
+            )
+
+    @debugmethod
+    def put_pixel(self, x, y, color):
+        out_of_bounds_in_x = (x < 0) or x >= self.w
+        out_of_bounds_in_y = (y < 0) or y >= self.h
+
+        if out_of_bounds_in_x or out_of_bounds_in_y:
+            return
+
+        self.pixels.put_color_at(x, y, color)

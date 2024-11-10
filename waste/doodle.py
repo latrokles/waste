08149be8898deeb89f074758b686ff040575e88c
latrokles <latrokles@gmail.com>
2024-11-10 12:42:31 -0800

    feat: add layout to doodle
    define a new canvas form where the drawing happens, add a toolbar
    section, perhaps a command section as well.

--- waste/doodle.py
+++ waste/doodle.py
@@ -10,6 +10,18 @@ def doodle():
     Doodle().run()
 
 
+PALETTE = [
+    draw.BLACK,
+    draw.DARK_YELLOW,
+    draw.PALE_BLUEGREEN,
+    draw.PALE_GREYGREEN,
+    draw.PALE_YELLOW,
+    draw.PURPLE_BLUE,
+    draw.YELLOW_GREEN,
+    draw.WHITE,
+]
+
+
 class Doodle(gui.Window):
     def __init__(self):
         super().__init__(
@@ -17,21 +29,37 @@ class Doodle(gui.Window):
             width=480,
             height=360,
             zoom=2,
-            background=draw.WHITE,
+            background=draw.PALE_YELLOW,
         )
-        self.pen = draw.Form(0, 0, 1, 1)
+        self.canvas = draw.Form(0, 0, 470, 300)
+        self.pen = draw.Form(0, 0, 10, 10)
         self.pen.fill(draw.BLACK)
-        self.clear()
+        self.clear_canvas()
+        self.redraw()
 
     def redraw(self):
+        self.draw_ui()
+        self.draw_canvas()
+
+    def draw_ui(self):
+        self.clear()
+        pass
+
+    def draw_canvas(self):
         if self.mouse.lb:
-            self.draw_line(self.mouse.prev_position, self.mouse.position, self.pen)
+            self.canvas.draw_line(
+                self.mouse.prev_position, self.mouse.position, self.pen
+            )
+        self.screen.bitblt(self.canvas, self.canvas.rect.clone(), draw.Point(5, 5))
 
     def on_key_down(self, key):
         if key == "q":
             self.quit()
         if key == gui.Modifier.ESC.name:
-            self.clear()
+            self.clear_canvas()
+
+    def clear_canvas(self):
+        self.canvas.fill(draw.WHITE)
 
 
 if __name__ == "__main__":
